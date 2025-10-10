6802f84b83b8d5136cddc71a56768af9dc93ce21
KK-Huang86 <iankk0806@gmail.com>
2025-10-10 17:32:11 +0800

    feat: add a view for linebot

--- linebot/views.py
+++ linebot/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings
+import logging
+import os
+
+
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
+)
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
+logger = logging.getLogger(__name__)
+
+# 初始化設定
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
+LINE_CHANNEL_SECRET = os.getenv('LINE_CHANNEL_SECRET')
+
+
+@csrf_exempt
+def callback(request):
+    if request.method != 'POST':
+        return HttpResponseBadRequest()
+
+    # 取得 X-Line-Signature header 值
+    signature = request.headers.get('X-Line-Signature', '')
+
+    # 取得 request body
+    body = request.body.decode('utf-8')
+    logger.info("Request body: " + body)
+
+    # 處理 webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        logger.error("Invalid signature. Please check your channel access token/channel secret.")
+        return HttpResponseBadRequest()
+
+    return HttpResponse('OK')
+
+
+@handler.add(MessageEvent, message=TextMessageContent)
+def handle_message(event):
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
