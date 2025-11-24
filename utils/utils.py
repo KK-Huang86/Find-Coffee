cb5bc26ffccc0b600320283db7d8747702234281
KK-Huang86 <iankk0806@gmail.com>
2025-11-24 09:56:29 +0800

    doc: name

--- utils/utils.py
+++ utils/utils.py
@@ -5,5 +5,5 @@ class LockService:
     def acquire(user_id, action, ttl=2):
         """防止使用者短時間內重複觸發同一個動作"""
 
-        key = f'ock:{user_id}:{action}'
+        key = f'lock:{user_id}:{action}'
         return cache.add(key, 1, timeout=ttl)  # SETNX + TTL
