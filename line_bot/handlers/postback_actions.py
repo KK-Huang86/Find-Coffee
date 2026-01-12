b2b6596bb06494a51e787fd204aa8f245006db6b
Kk <iankk0806@gmail.com>
2026-01-12 23:57:48 +0800

    refactor: use dict handle to refactor rich menu postback action

--- line_bot/handlers/postback_actions.py
+++ line_bot/handlers/postback_actions.py
@@ -116,64 +116,95 @@ def _handle_address_search(line_bot_api, reply_token, user, user_id, keyword):
     )
 
 
-def handle_menu(line_bot_api, reply_token, user, params):
-    """處理 Rich Menu 選單動作"""
-    menu_type = params.get('type')
+
+def _menu_search_shop_name(line_bot_api, reply_token, user):
+    """處理店名查詢"""
+    set_state(user.line_user_id, UserState.WAITING_SHOP_NAME)
+    reply_text(line_bot_api, reply_token, '請輸入咖啡店名稱 ☕️')
+
+
+def _menu_search_address(line_bot_api, reply_token, user):
+    """處理路名查詢"""
+    set_state(user.line_user_id, UserState.WAITING_ADDRESS)
+    reply_text(line_bot_api, reply_token, '請輸入路名️（例如：台北市信義區松信路）')
+
+
+def _menu_share_location(line_bot_api, reply_token, user):
+    """處理分享位置"""
+    quick_reply = QuickReplyBuilder.create_location_request()
+    line_bot_api.reply_message(
+        ReplyMessageRequest(
+            reply_token=reply_token,
+            messages=[
+                TextMessage(
+                    text='請點擊下方按鈕分享您的位置\n我來幫您找附近的咖啡店 ☕️',
+                    quick_reply=quick_reply
+                )
+            ]
+        )
+    )
+
+
+def _menu_favorites(line_bot_api, reply_token, user):
+    """處理收藏清單"""
     user_id = user.line_user_id
+    favorite_count = user.favorites.count()
 
-    if menu_type == MenuAction.SEARCH_SHOP_NAME:
-        set_state(user_id, UserState.WAITING_SHOP_NAME)
-        reply_text(line_bot_api, reply_token, '請輸入咖啡店名稱 ☕️')
+    if favorite_count == 0:
+        message = TextMessage(text='您還沒有收藏任何咖啡店喔～\n快去探索喜歡的店家吧！❤️')
+    elif favorite_count <= 5:
+        message = FavoritesMessageBuilder.show_favorites_carousel(user_id)
+    else:
+        message = FavoritesMessageBuilder.show_favorites_list(user_id)
 
-    elif menu_type == MenuAction.SEARCH_ADDRESS:
-        set_state(user_id, UserState.WAITING_ADDRESS)
-        reply_text(line_bot_api, reply_token, '請輸入路名️（例如：台北市信義區松信路）')
+    line_bot_api.reply_message(
+        ReplyMessageRequest(reply_token=reply_token, messages=[message])
+    )
 
-    elif menu_type == MenuAction.SHARE_LOCATION:
-        quick_reply = QuickReplyBuilder.create_location_request()
+
+def _menu_recent_search(line_bot_api, reply_token, user):
+    """處理最近查詢"""
+    user_id = user.line_user_id
+    quick_reply = QuickReplyBuilder.create_recent_search_quick_reply(user_id)
+
+    if quick_reply:
         line_bot_api.reply_message(
             ReplyMessageRequest(
                 reply_token=reply_token,
                 messages=[
-                    TextMessage(
-                        text='請點擊下方按鈕分享您的位置\n我來幫您找附近的咖啡店 ☕️',
-                        quick_reply=quick_reply
-                    )
+                    TextMessage(text='請選擇最近搜尋的關鍵字：', quick_reply=quick_reply)
                 ]
             )
         )
+    else:
+        reply_text(line_bot_api, reply_token, '目前還沒有搜尋紀錄喔～')
 
-    elif menu_type == MenuAction.FAVORITES:
-        favorite_count = user.favorites.count()
 
-        if favorite_count == 0:
-            message = TextMessage(text='您還沒有收藏任何咖啡店喔～\n快去探索喜歡的店家吧！❤️')
-        elif favorite_count <= 5:
-            message = FavoritesMessageBuilder.show_favorites_carousel(user_id)
-        else:
-            message = FavoritesMessageBuilder.show_favorites_list(user_id)
+def _menu_more_info(line_bot_api, reply_token, user):
+    """處理更多資訊，後續開發中"""
+    reply_text(line_bot_api, reply_token, '更多資訊功能開發中...')
 
-        line_bot_api.reply_message(
-            ReplyMessageRequest(reply_token=reply_token, messages=[message])
-        )
 
-    elif menu_type == MenuAction.RECENT_SEARCH:
-        quick_reply = QuickReplyBuilder.create_recent_search_quick_reply(user_id)
+# Menu Dispatch Table
+MENU_HANDLERS = {
+    MenuAction.SEARCH_SHOP_NAME: _menu_search_shop_name,
+    MenuAction.SEARCH_ADDRESS: _menu_search_address,
+    MenuAction.SHARE_LOCATION: _menu_share_location,
+    MenuAction.FAVORITES: _menu_favorites,
+    MenuAction.RECENT_SEARCH: _menu_recent_search,
+    MenuAction.MORE_INFO: _menu_more_info,
+}
 
-        if quick_reply:
-            line_bot_api.reply_message(
-                ReplyMessageRequest(
-                    reply_token=reply_token,
-                    messages=[
-                        TextMessage(text='請選擇最近搜尋的關鍵字：', quick_reply=quick_reply)
-                    ]
-                )
-            )
-        else:
-            reply_text(line_bot_api, reply_token, '目前還沒有搜尋紀錄喔～')
 
-    elif menu_type == MenuAction.MORE_INFO:
-        reply_text(line_bot_api, reply_token, '更多資訊功能開發中...')
+def handle_menu(line_bot_api, reply_token, user, params):
+    """處理 Rich Menu 選單動作（使用字典分派）"""
+    menu_type = params.get('type')
+    handler = MENU_HANDLERS.get(menu_type)
+
+    if handler:
+        handler(line_bot_api, reply_token, user)
+    else:
+        logger.warning(f'Unknown menu type: {menu_type}')
 
 
 # Action Dispatch Table
