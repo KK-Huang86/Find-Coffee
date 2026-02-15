1c248f124141a9b86b03fc37616ddab336051ace
Kk <iankk0806@gmail.com>
2026-02-15 17:33:33 +0800

    feat: add unit test for 'users' app

---
+++ users/tests/factories.py
@@ -0,0 +1,22 @@
+import factory
+
+from users.models import User, Friendship
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    line_user_id = factory.Sequence(lambda n: f'U{n:032d}')
+    display_name = factory.Faker('name', locale='zh_TW')
+    member_code = factory.Sequence(lambda n: f'M{n:05d}')
+    member_type = User.FREE
+    status = User.ACTIVE
+
+
+class FriendshipFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Friendship
+
+    user = factory.SubFactory(UserFactory)
+    friend = factory.SubFactory(UserFactory)
