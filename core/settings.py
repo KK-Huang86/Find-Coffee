30236c48e3d8929fd89b6f8832f4ac5d2f572500
KK <iankk0806@gmail.com>
2026-07-31 01:14:37 +0800

    feat: apply task of add-pgbouncer

--- core/settings.py
+++ core/settings.py
@@ -91,6 +91,7 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
         'PORT': config('DB_PORT'),
+        'CONN_MAX_AGE': 0,  # PgBouncer transaction mode 下必須為 0
     }
 }
 
